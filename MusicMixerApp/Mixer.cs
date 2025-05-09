3cc114102f09059988ab452a294818a0b10a28e1
Stanislav Cherkasov <chstas2002@gmail.com>
2025-05-09 20:10:44 +0700

    Минимальная реализация метода Mix для прохождения базового теста

---
+++ MusicMixerApp/Mixer.cs
@@ -0,0 +1,8 @@
+﻿public class Mixer
+{
+    public string Mix(string path1, string path2)
+    {
+        // TODO: реализовать полноценное объединение мелодий из файлов
+        return "combined.wav"; // временная заглушка
+    }
+}
